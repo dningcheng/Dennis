2b012976276ebcfebcde55de72f02e03271e7213
dnc <dningcheng@163.com>
2017-09-18 08:25:16 +0800

    清理了测试无用代码

--- springSpringMvcMybatis/src/main/java/com/seally/utils/UUIDGenerator.java
+++ springSpringMvcMybatis/src/main/java/com/seally/utils/UUIDGenerator.java
@@ -1,10 +1,7 @@
 package com.seally.utils;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Writer;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -25,22 +22,5 @@ public class UUIDGenerator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	public static void updateProperties(String key,String value){
-		properties.setProperty(key, value);
-		try {
-			Writer out = new FileWriter(new File(UUIDGenerator.class.getResource("/").getPath()+"test.properties"));
-			properties.store(out , "test update");
-			out.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public static void main(String[] args) {
-		UUIDGenerator.updateProperties("test", "nihao");
 	}
 }
