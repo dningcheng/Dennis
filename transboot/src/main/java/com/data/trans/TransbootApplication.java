28fabb7d30dedf92ed73a5a7a3e8d9b6371a3220
dnc <dningcheng@163.com>
2017-11-17 07:37:24 +0800

    init

---
+++ transboot/src/main/java/com/data/trans/TransbootApplication.java
@@ -0,0 +1,12 @@
+package com.data.trans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransbootApplication.class, args);
+	}
+}
