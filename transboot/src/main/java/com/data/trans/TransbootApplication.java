f5b7b9ff362c5414f6bd8e59f5f41687df2435b8
dengningcheng <dengningcheng@0easy.com>
2017-12-04 19:19:56 +0800

    注释

--- transboot/src/main/java/com/data/trans/TransbootApplication.java
+++ transboot/src/main/java/com/data/trans/TransbootApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @Date 2017年11月20日
+ * @author dnc
+ * 
+ */
 @SpringBootApplication
 @EnableTransactionManagement  //开启注解事务管理，等同于xml配置文件中的 <tx:annotation-driven />
 @MapperScan("com.data.trans.mapper")
