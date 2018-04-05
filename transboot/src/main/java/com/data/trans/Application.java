900f1ba6564ad6dcb26c652bbcc68e7a3850cec2
dennis <dennis@dningcheng>
2018-04-06 05:59:38 +0800

    配置精简

--- transboot/src/main/java/com/data/trans/Application.java
+++ transboot/src/main/java/com/data/trans/Application.java
@@ -15,12 +15,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement  //开启注解事务管理，等同于xml配置文件中的 <tx:annotation-driven />
 @MapperScan("com.data.trans.mapper") //mybatis的mapper接口包路径
-public class Application extends SpringBootServletInitializer{
+public class Application /*extends SpringBootServletInitializer*/{
 	
-	@Override
+	/*@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
+    }*/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
